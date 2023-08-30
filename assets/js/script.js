75b1820520d9dbabce8be76ed82b4a737f050388
Rafael Gregório de Oliveira <rafagre@live.com>
2023-08-30 20:57:17 -0300

    Atualização do menu

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+// ** Script do menu ** //
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu() {
+    const nav = document.getElementById('navegacao');
+    navegacao.classList.toggle('active');
+    const active = navegacao.classList.constain('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+
+
